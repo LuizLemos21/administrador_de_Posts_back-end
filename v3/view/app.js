1aab5f193979f9883c8821fb2bef6ef5911bb7c0
LuizLemos21 <104809823+LuizLemos21@users.noreply.github.com>
2024-05-13 14:12:32 -0300

    Reapply "front-end updates 12/05"
    This reverts commit 1b842c420d1de8d73b63fdd8a37cb86e4250bb0d.

--- v3/view/app.js
+++ v3/view/app.js
@@ -50,6 +50,9 @@ function fetchUsers() {
         users.forEach(user => {
             const li = document.createElement('li');
             li.textContent = `Name: ${user.nome}, Email: ${user.email}`;
+
+
+
             usersList.appendChild(li);
         });
     })
@@ -64,11 +67,63 @@ function fetchPosts() {
         postsList.innerHTML = ''; // Clear existing posts
         posts.forEach(post => {
             const li = document.createElement('li');
-            li.textContent = `Content: ${post.conteudo}, Data Agendada: ${post.dataagendamento}, Likes: ${post.likes}, Usuário: ${post.userid}`;
+            li.textContent = `Content: ${post.conteudo}, Data Agendada: ${post.dataagendamento}, Likes: ${post.likes}, Usuário: ${post.userid} `;
+                        
+            //Adicionar botões Edit e Delete
+            const editButton = document.createElement('button');
+            editButton.textContent='Edit';
+            editButton.onclick = () => editPost(post.id);
+            li.appendChild(editButton);
+
+            const deleteButton = document.createElement('button');
+            deleteButton.textContent='Delete';
+            deleteButton.onclick = () => deletePost(post.id);
+            li.appendChild(deleteButton);
+
+
             postsList.appendChild(li);
         });
     })
     .catch(error => console.error('Error fetching posts:', error));
+}
+
+function deletePost(id){
+    if (confirm("Tem certeza de que quer deletar este post?")){
+        fetch(`http://localhost:3000/post/${id}`, {
+            method: 'DELETE'
+        })
+        .then(response => response.json())
+        .then(data => {
+            alert('Post deletado!');
+            fetchPosts();
+        })
+        .catch(error => console.error('Ocorreu um erro ao deletar o post!:', error));
+    }
+
+}
+
+function editPost(id){
+    const newContent = prompt("Insira novo conteudo");
+    const newDate = prompt ("Insira nova data de agendamento");
+
+    const updatedPost = {
+        conteudo: newContent,
+        dataagendamento: newDate
+    };
+
+    fetch(`http://localhost:3000/post/${id}`,{
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(updatedPost)
+    })
+    .then(response => response.json())
+    .then(data => {
+        alert('Post atualizado');
+        fetchPosts();
+    })
+    .catch(error => console.error ('Erro ao atualizar post:', error));
 }
 
 // Fetch data initially on page load
